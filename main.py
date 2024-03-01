9a11255de687983460a638d7cffb5c2bcca83659
MPpennings <161813291+MPpennings@users.noreply.github.com>
2024-03-01 08:29:11 +0000

    high scores toegevoegd

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+score = random.randint(1,100)
+score2 = random.randint(0,100)
 print("Hallo, Heerlijke Ballen!")
 print("wil jij ook kont?")
-print("sillebil eet pik")
-print("Señor maarten is geil")
-
+print("sillebil eet",score ,"pikken")
+print("Señor maarten is ",score2,"keer geil")
